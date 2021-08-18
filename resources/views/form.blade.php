1ebb12d37999fcc8b3820532c68580b7e690d959
tuan <tuan.le@codegym.vn>
2021-08-18 12:06:47 +0700

    first commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+Form mẫu kiểm tra CSRF
+<form method="post" action="{{ route('submit') }}">
+    {{ csrf_field() }}
+    <input name="name" placeholder="Điền thông tin">
+    <input type="submit" value="Chấp nhận">
+</form>
+</body>
+</html>
